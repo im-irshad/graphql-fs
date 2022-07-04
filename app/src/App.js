71555ddb7c7f4b79c55f69b17c5fc6118521b631
Irshad Muhammad <imabbasi@msn.com>
2022-07-04 13:19:04 +0200

    added client components & configure apollo client in app file

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Header from "./components/Header";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Clients from "./components/Clients";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Container className="p-3">
-      <Header />
-    </Container>
+    <>
+      <ApolloProvider client={client}>
+        <Container className="p-3">
+          <Header />
+          <Clients />
+        </Container>
+      </ApolloProvider>
+    </>
   );
 }
 
