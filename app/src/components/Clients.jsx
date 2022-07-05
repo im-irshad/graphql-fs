1e7f6914d7ec14deae784e95aca0c5ef2d6ebf67
Irshad Muhammad <imabbasi@msn.com>
2022-07-05 19:52:47 +0200

    presented data from api in bootstrap table

--- app/src/components/Clients.jsx
+++ app/src/components/Clients.jsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-
+import Table from "react-bootstrap/Table";
+import { MdDelete } from "react-icons/md";
 const GET_CLIENTS = gql`
   query getClients {
     clients {
@@ -18,12 +19,33 @@ export default function Clients() {
 
   return (
     <div>
-      <h1>Clients</h1>
-      <ul>
-        {data.clients.map((client) => (
-          <li key={client.id}>{client.name}</li>
-        ))}
-      </ul>
+      {!loading && !error && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <tr key={client.id}>
+                <td>{client.id}</td>
+                <td>{client.name}</td>
+                <td>{client.email}</td>
+                <td>{client.phone}</td>
+                <td>
+                  {" "}
+                  <MdDelete />{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 }
